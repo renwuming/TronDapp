31c7b96a43e526919fde7c3dadc8ac5e53954622
wkorz <wkorz@pku.edu.cn>
2018-12-28 21:58:55 +0800

    length++

--- src/App.js
+++ src/App.js
@@ -35,9 +35,24 @@ class App extends Component {
     }
 
 
-    //检查是否有用户数据
-    //true --- 有数据 ，调用 predict
-    //false -- 没有数据，调用 predictFirst
+    ///简介
+    ///这个项目主要功能  算命～～～～～，分为爱情，事业，财富
+
+    ///1.checkUser,用来检查用户是否以前算过，算过的话，链上有用户数据，就不需要重新输入数据
+    ///            没有算过的话，就需要输入个人信息
+    ///每次只能选一个算，爱情事业财富，三选一
+    ///如果之前算过爱情的，再算爱情的，结果不会改变
+    ///想要改变结果需要通过转运功能
+
+    ////用户可以做的事情  1. 爱情，事业，财富   算命
+    ////               2. 爱情，事业，财富   转运
+    ////               3. 打赏开发者
+    
+    
+    //checkUser检查是否有用户数据（返回true 或者 false）
+    //typeid 预测的类型（1 -- 爱情， 2 -- 事业， 3 -- 财运）
+    //true --- 有数据 ，调用 predict (只需要传入一个参数，typeid)
+    //false -- 没有数据，调用 predictFirst （除了typeid， 还有个人信息）
     checkUser = async () => {
         let result_0 = await this.contract.checkUser().call()
         console.log(result_0)
@@ -45,72 +60,80 @@ class App extends Component {
     };
 
     //我的记录
+    //获取用户数据
+    /*
+    struct User {
+        uint256 userid;             //用户id
+        string name;                //姓名
+        uint256 sex;                //性别 0--女， 1--- 男
+        uint256 birthday;           // 19990110
+        uint256 luckynumber;        //6666
+        uint256 type_love;          //99    -- 用分数和类型去大苏那里去文字
+        uint256 type_career;        //66
+        uint256 type_money;         //88
+    }
+    */
     getUser = async () => {
         let result_0 = await this.contract.getUser().call()
-        console.log(result_0)
-        let love = result_0[5].length
-        let listlove = []
-        for(let i = 0; i < love; i++) {
-            listlove.push(JSON.parse(result_0[5][i]))
-        }
-        let career = result_0[6].length
-        let listcareer = []
-        for(let i = 0; i < career; i++) {
-            listcareer.push(JSON.parse(result_0[6][i]))
-        }
-        let money = result_0[7].length
-        let listmoney = []
-        for(let i = 0; i < money; i++) {
-            listmoney.push(JSON.parse(result_0[7][i]))
-        }
+        console.log(result_0);
         this.setState({getUserkey:JSON.parse(result_0[0])})
         this.setState({getUsername:result_0[1]})
         this.setState({getUsersex:JSON.parse(result_0[2])})
         this.setState({getUserbirthday:JSON.parse(result_0[3])})
         this.setState({getUserluckynumber:JSON.parse(result_0[4])})
-        this.setState({getUserlove:listlove.join(",")})
-        this.setState({getUsercareer:listcareer.join(",")})
-        this.setState({getUsermoney:listmoney.join(",")})
+        this.setState({getUserlove:JSON.parse(result_0[5])})
+        this.setState({getUsercareer:JSON.parse(result_0[6])})
+        this.setState({getUsermoney:JSON.parse(result_0[7])})
     };
 
 
     //运势
     predict = async () => {
         //监听
+        //41dbeba2b4d7e5ce37f84e6b949681316c8259a159 为合约地址，部署新合约后，需替换
+        let score;
         console.log("---------------------")
-        let contractInstance = await tronWeb.contract().at("41dbeba2b4d7e5ce37f84e6b949681316c8259a159");
+        let contractInstance = await tronWeb.contract().at("412cbec6c38a6fb76a315235c9b4989396d65de36e");
         console.log(contractInstance)
+        //监听 predictSuccess 事件
         contractInstance["predictSuccess"]().watch(function(err, res) {
             console.log("error " + err);
             console.log('eventResult:',res);
             console.log("---------------------")
-            console.log('Money:',res["result"]["score"]);
+            console.log('Money:',res["result"]["score"]);//监听结果，为预测的分数
+            score = res["result"]["score"];
             //this.setState({predict:res["result"]["score"]})
         });
         console.log("---------------------")
         //调用 合约中得predict
         let typepredict = 1 ///1是love，2是career，3是money
-        let result_0 = await this.contract.predict(typepredict).send({
-            feeLimit:100000000,
-            callValue:0,
-            shouldPollResponse:true
-        })
+        let result_0 = await this.contract.predict(typepredict).call()
         console.log(result_0)
-        
+        score = JSON.parse(result_0)
+        if(score == 0) {
+            let result_0 = await this.contract.predictType(typepredict).send({
+                feeLimit:100000000,
+                callValue:0,
+                shouldPollResponse:true
+            })
+            console.log(result_0)
+        }
+        this.setState({predict:score})
     };
 
     //第一次算运势
     predictFirst = async () => {
         //监听
+        let score;
         console.log("---------------------")
-        let contractInstance = await tronWeb.contract().at("41dbeba2b4d7e5ce37f84e6b949681316c8259a159");
+        let contractInstance = await tronWeb.contract().at("412cbec6c38a6fb76a315235c9b4989396d65de36e");
         console.log(contractInstance)
         contractInstance["predictSuccess"]().watch(function(err, res) {
             console.log("error " + err);
             console.log('eventResult:',res);
             console.log("---------------------")
-            console.log('Money:',res["result"]["score"]);
-            this.setState({predictFirst:res["result"]["score"]})
+            console.log('score:',res["result"]["score"]);
+            score = res["result"]["score"];
         });
         console.log("---------------------")
         //调用 合约中得predictFirst
@@ -126,43 +149,14 @@ class App extends Component {
             shouldPollResponse:true
         })
         console.log(result_0)
-    };
-
-    //为他人算运势
-    predictForothers = async () => {
-        //监听
-        console.log("---------------------")
-        let contractInstance = await tronWeb.contract().at("41dbeba2b4d7e5ce37f84e6b949681316c8259a159");
-        console.log(contractInstance)
-        contractInstance["predictSuccess"]().watch(function(err, res) {
-            console.log("error " + err);
-            console.log('eventResult:',res);
-            console.log("---------------------")
-            console.log('Money:',res["result"]["score"]);
-            this.setState({predictForothers:res["result"]["score"]})
-        });
-        console.log("---------------------")
-        //调用 合约中得predict
-        //string _name, uint256 _sex, uint256 _birthday, uint256 _luckynumber, uint256 _type
-        let name = "hahaha"
-        let sex = 1
-        let birthday = 19970101
-        let luckynumber = 6
-        let typepredict = 2 ///1是love，2是career，3是money
-        let result_0 = await this.contract.predictForothers(name, sex, birthday, luckynumber,typepredict).send({
-            feeLimit:100000000,
-            callValue:0,
-            shouldPollResponse:true
-        })
-        console.log(result_0)
-        
+        this.setState({predictFirst:score})
     };
 
     //捐赠给开发者
     donate = async () => {
         //监听
         console.log("---------------------")
-        let contractInstance = await tronWeb.contract().at("41dbeba2b4d7e5ce37f84e6b949681316c8259a159");
+        let contractInstance = await tronWeb.contract().at("412cbec6c38a6fb76a315235c9b4989396d65de36e");
         console.log(contractInstance)
         contractInstance["DonateSuccess"]().watch(function(err, res) {
             console.log("error " + err);
@@ -185,62 +179,79 @@ class App extends Component {
     //转运
     help = async () => {
         //监听
+        let score;
         console.log("---------------------")
-        let contractInstance = await tronWeb.contract().at("41dbeba2b4d7e5ce37f84e6b949681316c8259a159");
+        let contractInstance = await tronWeb.contract().at("412cbec6c38a6fb76a315235c9b4989396d65de36e");
         console.log(contractInstance)
-        contractInstance["HelpSuccess"]().watch(function(err, res) {
+        contractInstance["predictSuccess"]().watch(function(err, res) {
             console.log("error " + err);
             console.log('eventResult:',res);
             console.log("---------------------")
-            console.log('Money:',res["result"]["money"]);
+            console.log('score:',res["result"]["score"]);
+            score = res["result"]["score"]
         });
         console.log("---------------------")
-        let result_0 = await this.contract.help().send({
+        let typeid = 1
+        let result_0 = await this.contract.help(typeid).send({
             feeLimit:100000000,
-            callValue:5000000,
+            callValue:5000000,      //手续费，转运付费，5tron
             shouldPollResponse:true
         })
         console.log(result_0)
-        this.setState({help:res["result"]["money"]})
+        this.setState({help:score})
     };
 
-    //根据typeid和id获取记录
-    getRecord = async () => {
-        let result_0 = await this.contract.getRecord(2,1).call()
-        console.log(result_0)
-        this.setState({getRecordid:JSON.pares(result_0[0])})
-        this.setState({getRecordscore:JSON.pares(result_0[1])})
-        this.setState({getRecordowner:JSON.pares(result_0[2])})
-    }
-
+    //不做锦鲤榜的话，下面的getLove，getCareer，getMoney就不用了
     //爱情运势记录，typeid = 1
+    //用来坐排行榜
     getLove = async () => {
         let result_0 = await this.contract.getLovekey().call()
+        let users = [];
+        let user;
+        console.log("----------------")
         console.log(result_0)
-        for(let i = 1; i<result_0; i++) {
-            console.log(await this.contract.getRecord(1,i).call())
+        console.log("----------------")
+        for(let i = 1; i<JSON.parse(result_0); i++) {
+            console.log(i)
+            user = console.log(await this.contract.getLove(i).call())
+            //返回这预测的用户
+            //用用户地址去取他的分数，
+            users.push(user)
         }
-        this.setState({getLove:result_0[0]})
+        this.setState({getLove:users.join(",")})
     }
 
     //事业运势记录，typeid = 2
     getCareer = async () => {
         let result_0 = await this.contract.getCareerkey().call()
+        let users = [];
+        let user;
+        console.log("----------------")
         console.log(result_0)
+        console.log("----------------")
         for(let i = 1; i<result_0; i++) {
-            console.log(await this.contract.getRecord(2,i).call())
+            console.log(i)
+            console.log(await this.contract.getCareer(i).call())
+            users.push(user)
         }
-        this.setState({getCareer:JSON.pares(result_0)})
+        this.setState({getCareer:users.join(",")})
     }
 
     //财富运势记录，typeid = 3
     getMoney = async () => {
         let result_0 = await this.contract.getMoneykey().call()
+        let users = [];
+        let user;
+        console.log("----------------")
+        console.log(result_0)
+        console.log("----------------")
         console.log(result_0)
         for(let i = 1; i<result_0; i++) {
-            console.log(await this.contract.getRecord(3,i).call())
+            console.log(i)
+            console.log(await this.contract.getMoney(i).call())
+            users.push(user)
         }
-        this.setState({getMoney:JSON.pares(result_0)})
+        this.setState({getMoney:users.join(",")})
     }
 
 
@@ -290,34 +301,19 @@ class App extends Component {
                 </div>
 
                 <div>
-                    <p>predictForothers</p>
-                    <p>分数：{this.state.predictFirst}</p>
-                    <button onClick={this.predictFirst}>predictFirst</button>
-                    <hr></hr>
-                </div>
-
-                <div>
                     <p>donate</p>
-                    <p>捐赠金额{this.state.donate}</p>
+                    <p>捐赠金额：{this.state.donate}</p>
                     <button onClick={this.donate}>donate</button>
                     <hr></hr>
                 </div>
 
                 <div>
                     <p>help</p>
-                    <p>转运支付的金额{this.state.help}</p>
+                    <p>转运后分数：{this.state.help}</p>
                     <button onClick={this.help}>help</button>
                     <hr></hr>
                 </div>
 
-                <div>
-                    <p>getRecord</p>
-                    <p>ID：{this.state.getRecordid}</p>
-                    <p>Owner：{this.state.getRecordscore}</p>
-                    <p>score{this.state.getRecordowner}</p>
-                    <button onClick={this.getRecord}>getRecord</button>
-                    <hr></hr>
-                </div>
 
                 <div>
                     <p>getLove</p>
